03a585c0e667594749a369a1f5d15d6dccb979d5
OrigamiStarz <isabelbku@gmail.com>
2023-01-02 13:46:42 -0500

    created script.js, has button functionality

---
+++ script.js
@@ -0,0 +1,13 @@
+const btnID = "startbutton";
+const diceID = "dicefaces";
+const btn = document.getElementById(btnID);
+const dice = document.getElementById(diceID);
+
+btn.addEventListener("click", function() {
+    const randomNumber = Math.floor(Math.random()*6) + 1; 
+    if (dice.children.length === 0) {
+        const p = document.createElement("p");
+        dice.append(p);
+    }
+    diceNumber.innerHTML = randomNumber; 
+});
