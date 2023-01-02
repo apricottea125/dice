fb0cdd39b95d31055afb80461a578bebb106ddda
OrigamiStarz <40199724+OrigamiStarz@users.noreply.github.com>
2023-01-02 15:18:08 -0800

    Added transitions to dice and centered button/dice

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const btnID = "startbutton";
-const diceID = "dicefaces";
 const btn = document.getElementById(btnID);
-const dice = document.getElementById(diceID);
 
 btn.addEventListener("click", function() {
     const randomNumber = Math.floor(Math.random()*6) + 1; 
     const allDiceIDs = ["one","two","three","four","five","six"];
     for (let i=0; i<allDiceIDs.length; i++) {
         document.getElementById(allDiceIDs[i]).style.opacity = "0";
+        document.getElementById(allDiceIDs[i]).style.transform = "rotate(0deg)";
     }
     document.getElementById(allDiceIDs[randomNumber - 1]).style.opacity = "1";
+    document.getElementById(allDiceIDs[randomNumber - 1]).style.transform = "rotate(360deg)";
 });
 
 const randomNumber = Math.floor(Math.random()*6) + 1; 
